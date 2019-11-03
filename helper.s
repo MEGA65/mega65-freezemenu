4331bb973c105edd1222913728b63e5d86c65166
Paul Gardner-Stephen <paul@servalproject.org>
2019-11-03 17:12:47 +1030

    add wrapper routine for dos_trap_loadfile

--- helper.s
+++ helper.s
@@ -12,17 +12,89 @@
 	
 .SEGMENT "CODE"
 
+	.p4510
+	
+_mega65_dos_exechelper:
+	;; char mega65_dos_exechelper(char *image_name);
+
+	;; Get pointer to file name
+	;; sp here is the ca65 sp ZP variable, not the stack pointer of a 4510
+	ldy #1
+	.p02
+	lda (sp),y
+	sta ptr1+1
+	sta $0141
+	dey
+	lda (sp),y
+	.p4510
+	sta ptr1
+	sta $0140
+	
+	;; Copy file name
+	ldy #0
+@NameCopyLoop:
+	lda (ptr1),y
+	sta $0100,y
+	iny
+	cmp #0
+	bne @NameCopyLoop
+	
+	;;  Call dos_setname()
+	ldy #>$0100
+	ldx #<$0100
+	lda #$2E     		; dos_setname Hypervisor trap
+	STA $D640		; Do hypervisor trap
+	NOP			; Wasted instruction slot required following hyper trap instruction
+	;; XXX Check for error (carry would be clear)
+
+	; close all files to work around hyppo file descriptor leak bug
+        lda #$22
+        sta $d640
+        nop
+	
+	;; Now copy a little routine into place in $0340 that does the actual loading and jumps into
+	;; the program when loaded.
+	ldx #$00
+@lfr1:	lda loadfile_routine,x
+	sta $0340,x
+	inx
+	cpx #$80
+	bne @lfr1
+
+	;; Call helper routine
+	jsr $0340
+	
+	;; as this is effectively like exec() on unix, it can only return an error
+	LDA #$01
+	LDX #$00
+	
+	RTS
+
+loadfile_routine:
+	; Now load the file to $0400 over the screen
+        lda #$36
+        ldx #$FF
+        ldy #$07
+        ldz #$00
+        sta $d640
+        nop
+        ldz     #$00
+	jmp $080d
+	rts
+	
 _mega65_dos_attachd81:
 	;; char mega65_dos_attachd81(char *image_name);
 
 	;; Get pointer to file name
 	;; sp here is the ca65 sp ZP variable, not the stack pointer of a 4510
 	ldy #1
+	.p02
 	lda (sp),y
 	sta ptr1+1
 	sta $0441
 	dey
 	lda (sp),y
+	.p4510
 	sta ptr1
 	sta $0440
 	
@@ -63,7 +135,7 @@ _unfreeze_slot:
 	PHX
 	TAY
 	PLA
-	TAX	
+	TAX
 
 	;; Call hypervisor trap
 	LDA #$12    ; subfunction for syspart trap to unfreeze from a slot
@@ -129,10 +201,12 @@ _read_file_from_sdcard:
 	;; Get pointer to file name
 	;; sp here is the ca65 sp ZP variable, not the stack pointer of a 4510
 	ldy #5
+	.p02
 	lda (sp),y
 	sta ptr1+1
 	dey
 	lda (sp),y
+	.p4510
 	sta ptr1
 
 	;; Copy file name
@@ -154,15 +228,17 @@ _read_file_from_sdcard:
 
 	;; Get Load address into $00ZZYYXX
 	ldy #2
-	lda (sp),y
-	.p4510	
-	taz
 	.p02
+	lda (sp),y
+	.p4510
+	taz
 	ldy #0
+	.p02
 	lda (sp),y
 	tax
 	iny
 	lda (sp),y
+	.p4510
 	tay
 
 	;; Ask hypervisor to do the load
@@ -171,9 +247,7 @@ _read_file_from_sdcard:
 	NOP
 	;; XXX Check for error (carry would be clear)
 
-	.p4510
 	LDZ #$00
-	.p02
 	
 	RTS
 
@@ -219,7 +293,6 @@ _readdir:
 	;; Third, call the hypervisor trap
 	;; File descriptor gets passed in in X.
 	;; Result gets written to transfer area we setup at $0400
-	.p4510
 	plx
 	ldy #>$0400 		; write dirent to $0400 
 	lda #$14
