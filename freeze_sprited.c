dd0ff2a9089527cea745a3a9d37db7e6f0fc41db
Paul Gardner-Stephen <paul@servalproject.org>
2020-07-01 16:22:06 +0930

    fix mouse positioning etc

--- freeze_sprited.c
+++ freeze_sprited.c
@@ -445,9 +445,9 @@ void mouse_bind_to_sprite(unsigned char sprite_num)
 void mouse_clip_position(void)
 {
   if (mouse_x<mouse_min_x) mouse_x=mouse_min_x;
-  if (mouse_y<mouse_min_y) mouse_y=mouse_min_x;
+  if (mouse_y<mouse_min_y) mouse_y=mouse_min_y;
   if (mouse_x>mouse_max_x) mouse_x=mouse_max_x;
-  if (mouse_y>mouse_max_y) mouse_y=mouse_max_x;
+  if (mouse_y>mouse_max_y) mouse_y=mouse_max_y;
 }
 
 char mouse_clicked(void)
@@ -467,12 +467,18 @@ void mouse_update_pointer(void)
     else  POKE(0xD010,PEEK(0xD010)&(0xFF-(1<<mouse_sprite_number)));
     if (mouse_x&0x200) POKE(0xD05F,PEEK(0xD05F)|(1<<mouse_sprite_number));
     else  POKE(0xD05F,PEEK(0xD05F)&(0xFF-(1<<mouse_sprite_number)));
+
     POKE(0xD001+(mouse_sprite_number<<1),mouse_y&0xff);
     if (mouse_y&0x100) POKE(0xD077,PEEK(0xD077)|(1<<mouse_sprite_number));
     else  POKE(0xD077,PEEK(0xD077)&(0xFF-(1<<mouse_sprite_number)));
     if (mouse_y&0x200) POKE(0xD05F,PEEK(0xD05F)|(1<<mouse_sprite_number));
     else  POKE(0xD078,PEEK(0xD078)&(0xFF-(1<<mouse_sprite_number)));
   }
+
+  gotoxy(0,0);
+  cputdec(mouse_x, 0, 0);
+  gotoxy(0,1);
+  cputdec(mouse_y, 0, 0);
 }
 
 void mouse_update_position(unsigned short *mx,unsigned short *my)
@@ -484,11 +490,11 @@ void mouse_update_position(unsigned short *mx,unsigned short *my)
   delta=-delta;
   if (delta>=0x01&&delta<=0x3f) mouse_x-=delta;
 
-  delta=PEEK(0xD620)-mouse_pot_y;
-  mouse_pot_y=PEEK(0xD620);
-  if (delta>=0x01&&delta<=0x3f) mouse_y+=delta;
-  delta=-delta;
+  delta=PEEK(0xD621)-mouse_pot_y;
+  mouse_pot_y=PEEK(0xD621);
   if (delta>=0x01&&delta<=0x3f) mouse_y-=delta;
+  delta=-delta;
+  if (delta>=0x01&&delta<=0x3f) mouse_y+=delta;
   
   mouse_clip_position();
   mouse_update_pointer();
@@ -519,8 +525,8 @@ static void MainLoop()
     BYTE redrawCanvas = FALSE;
     BYTE redrawTools = FALSE;
 
-    mouse_set_bounding_box(0,0,319,199);
-    mouse_warp_to(160,100);
+    mouse_set_bounding_box(0+24,0+50,319+24-8,199+50);
+    mouse_warp_to(24,100);
     mouse_bind_to_sprite(0);
     
     while (1)
