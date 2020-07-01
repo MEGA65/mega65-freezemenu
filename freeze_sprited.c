f46db570ca696aa1ff5afbba5cab1972d6a7ae92
Paul Gardner-Stephen <paul@servalproject.org>
2020-07-01 16:07:35 +0930

    begin implementing mouse reading functions

--- freeze_sprited.c
+++ freeze_sprited.c
@@ -97,7 +97,7 @@ static void Initialize()
     POKE(0xd02fL, 0x53);
 
     // Set sprite 0 to our cursor
-    lcopy(sprite_pointer,0x380,63);
+    lcopy((long)sprite_pointer,0x380,63);
     POKE(0xD015,1);
     POKE(0x07F8,0x380/64);
     POKE(0xD000,100);
@@ -416,6 +416,101 @@ unsigned short joy_delay_countdown = 0;
 unsigned short joy_delay = 10000;
 unsigned char fire_lock = 0;
 
+unsigned short mx,my;
+
+unsigned short mouse_min_x=0;
+unsigned short mouse_min_y=0;
+unsigned short mouse_max_x=319;
+unsigned short mouse_max_y=199;
+unsigned short mouse_x=0;
+unsigned short mouse_y=0;
+unsigned char mouse_sprite_number=0xff;
+unsigned char mouse_pot_x=0;
+unsigned char mouse_pot_y=0;
+char mouse_click_flag=0;
+
+void mouse_set_bounding_box(unsigned short x1, unsigned short y1,unsigned short x2,unsigned short y2)
+{
+  mouse_min_x=x1;
+  mouse_min_y=y1;
+  mouse_max_x=x2;
+  mouse_max_y=y2;  
+}
+
+void mouse_bind_to_sprite(unsigned char sprite_num)
+{
+  mouse_sprite_number=sprite_num;
+}
+
+void mouse_clip_position(void)
+{
+  if (mouse_x<mouse_min_x) mouse_x=mouse_min_x;
+  if (mouse_y<mouse_min_y) mouse_y=mouse_min_x;
+  if (mouse_x>mouse_max_x) mouse_x=mouse_max_x;
+  if (mouse_y>mouse_max_y) mouse_y=mouse_max_x;
+}
+
+char mouse_clicked(void)
+{
+  if (!(PEEK(0xDC01)&0x80)) mouse_click_flag=1;
+  if (mouse_click_flag) {
+    mouse_click_flag=0;
+    return 1;
+  }
+}
+
+void mouse_update_pointer(void)
+{
+  if (mouse_sprite_number<8) {
+    POKE(0xD000+(mouse_sprite_number<<1),mouse_x&0xff);
+    if (mouse_x&0x100) POKE(0xD010,PEEK(0xD010)|(1<<mouse_sprite_number));
+    else  POKE(0xD010,PEEK(0xD010)&(0xFF-(1<<mouse_sprite_number)));
+    if (mouse_x&0x200) POKE(0xD05F,PEEK(0xD05F)|(1<<mouse_sprite_number));
+    else  POKE(0xD05F,PEEK(0xD05F)&(0xFF-(1<<mouse_sprite_number)));
+    POKE(0xD001+(mouse_sprite_number<<1),mouse_y&0xff);
+    if (mouse_y&0x100) POKE(0xD077,PEEK(0xD077)|(1<<mouse_sprite_number));
+    else  POKE(0xD077,PEEK(0xD077)&(0xFF-(1<<mouse_sprite_number)));
+    if (mouse_y&0x200) POKE(0xD05F,PEEK(0xD05F)|(1<<mouse_sprite_number));
+    else  POKE(0xD078,PEEK(0xD078)&(0xFF-(1<<mouse_sprite_number)));
+  }
+}
+
+void mouse_update_position(unsigned short *mx,unsigned short *my)
+{
+  unsigned char delta;
+  delta=PEEK(0xD620)-mouse_pot_x;
+  mouse_pot_x=PEEK(0xD620);
+  if (delta>=0x01&&delta<=0x3f) mouse_x+=delta;
+  delta=-delta;
+  if (delta>=0x01&&delta<=0x3f) mouse_x-=delta;
+
+  delta=PEEK(0xD620)-mouse_pot_y;
+  mouse_pot_y=PEEK(0xD620);
+  if (delta>=0x01&&delta<=0x3f) mouse_y+=delta;
+  delta=-delta;
+  if (delta>=0x01&&delta<=0x3f) mouse_y-=delta;
+  
+  mouse_clip_position();
+  mouse_update_pointer();
+
+  if (!(PEEK(0xDC01)&0x80)) mouse_click_flag=1;
+  
+  if (mx) *mx=mouse_x;
+  if (my) *my=mouse_y;
+}
+
+void mouse_warp_to(unsigned short x,unsigned short y)
+{
+  mouse_x=x;
+  mouse_y=y;
+  mouse_clip_position();
+  mouse_update_pointer();
+
+  // Mark POT position as read
+  mouse_pot_x=PEEK(0xD620);
+  mouse_pot_y=PEEK(0xD621);
+}
+
 static void MainLoop()
 {
     FILEOPTIONS fileOpt;
@@ -424,8 +519,16 @@ static void MainLoop()
     BYTE redrawCanvas = FALSE;
     BYTE redrawTools = FALSE;
 
+    mouse_set_bounding_box(0,0,319,199);
+    mouse_warp_to(160,100);
+    mouse_bind_to_sprite(0);
+    
     while (1)
     {
+      mouse_update_position(&mx,&my);
+      if (mouse_clicked()) {
+      }
+      
       if (kbhit())
 	{
 	  key = cgetc();
