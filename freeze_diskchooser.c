ba40ee4544fb82a5ddd36d8a7f5dca74460a712b
johnwayner <johnwayner@users.noreply.github.com>
2022-06-13 01:40:04 -0500

    Remove infinite loops when attempting to clear "use real drive" flags (#36)
    * Remove infinite loops when attempting to clear "use real drive" flags
    
    Must be in hypervisor mode to clear the use real floppy flags.  So this will not work.
    
    They sometimes appear to work due to a hypervisor trap having earlier set the flags to zero (to show a .d81 listing).
    
    * Remove JUST the infinite while loops.

--- freeze_diskchooser.c
+++ freeze_diskchooser.c
@@ -468,19 +468,10 @@ char* freeze_select_disk_image(unsigned char drive_id)
       if (drive_id == 0) {
         // Clear flags for drive 0
         lpoke(0xffd368bL, lpeek(0xffd368bL) & 0xb8);
-
-        // there seem to be an issue reliably using lpoke/lpeek here
-        // so for now we repeat to ensure we have what we want
-        while (lpeek(0xffd36a1L) & 1) {
-          lpoke(0xffd36a1L, lpeek(0xffd36a1L) & 0xfe);
-        }
       }
       else if (drive_id == 1) {
         // Clear flags for drive 1
         lpoke(0xffd368bL, lpeek(0xffd368bL) & 0x47);
-        while (lpeek(0xffd36a1L) & 4) {
-          lpoke(0xffd36a1L, lpeek(0xffd36a1L) & 0xfb);
-        }
       }
 
       // Try to mount it, with border black while working
