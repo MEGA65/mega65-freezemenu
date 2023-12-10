02f8b55cf7f164d86ff7cfb47476ee8470d555cb
Oliver Graf <ograf@devilsclaw.de>
2023-12-10 21:19:27 +0100

    use git branch --remote to determine branch (might be detached)

--- gitversion.sh
+++ gitversion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # status of 'B'ranch in 'S'hort format
-branch=`git rev-parse --abbrev-ref HEAD`
+branch=`git branch --remote --verbose --no-abbrev --contains | sed -rne 's/^[^\/]*\/([^\ ]+).*$/\1/p'`
 # get from charpos3, for 6 chars
 branch2=${branch:0:6}
 version=`git describe --always --abbrev=7 | sed -e 's/(//g' -e 's/)//g' -e's/ /_/g'`
