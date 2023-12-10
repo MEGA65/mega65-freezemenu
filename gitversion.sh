b381d81aab7e4610a14b607083bb917b1ef04df7
Oliver Graf <ograf@devilsclaw.de>
2023-12-10 20:00:50 +0100

    change version script to use rev-parse

--- gitversion.sh
+++ gitversion.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # status of 'B'ranch in 'S'hort format
-branch=`git status -b -s | head -n 1 | sed -e 's/(//g' -e 's/)//g' -e's/ /_/g'`
+branch=`git rev-parse --abbrev-ref HEAD`
 # get from charpos3, for 6 chars
-branch2=${branch:3:6}
+branch2=${branch:0:6}
 version=`git describe --always --abbrev=7 | sed -e 's/(//g' -e 's/)//g' -e's/ /_/g'`
 
 datetime=`date +%Y%m%d.%H`
