e9c9fe7183834864cd57f7a60cbaff52a61b7d58
Oliver Graf <oliver@m-e-g-a.org>
2024-01-03 22:23:57 +0100

    add special short branch name generation to gitversion.sh

--- gitversion.sh
+++ gitversion.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+shorten_name () {
+  name=$1
+  # issue branch?
+  if [[ $name =~ ^([0-9]+)-(.+)$ ]]; then
+    name="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
+  fi
+  # developemnt + extra?
+  if [[ $name =~ ^development-(.+)$ ]]; then
+    name="dev${BASH_REMATCH[1]}"
+  fi
+  # release + version?
+  if [[ $name =~ ^release-([0-9]\.[0-9]\.[0-9])$ ]]; then
+    name="r${BASH_REMATCH[1]}"
+  fi
+  # cut after 6 chars
+  echo ${name:0:6}
+}
+
 # status of 'B'ranch in 'S'hort format
 branch=`git branch --remote --verbose --no-abbrev --contains | sed -rne 's/^[^\/]*\/([^\ ]+).*$/\1/p'`
-# get from charpos3, for 6 chars
-branch2=${branch:0:6}
+branch2=$(shorten_name $branch)
 version=`git describe --always --abbrev=7 | sed -e 's/(//g' -e 's/)//g' -e's/ /_/g'`
 
 datetime=`date +%Y%m%d.%H`
